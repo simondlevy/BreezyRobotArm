d06d0f7f36572c2ed8f042fb5b6be920bdb00993
simondlevy <simon.d.levy@gmail.com>
2017-05-20 21:05:52 -0400

    Added pydoc

--- breezyrobotarm/__init__.py
+++ breezyrobotarm/__init__.py
@@ -20,8 +20,17 @@ along with this code.  If not, see <http:#www.gnu.org/licenses/>.
 '''
 
 class Joint(object):
+    '''
+    A class for controlling a single joint using a servo motor.
+    '''
 
     def __init__(self, board, pin, start):
+        '''
+        Creates a new Joint object.
+        board - a PyFirmata Board object
+        pin   - Arduino pin to which the servo is connected
+        start - initial servo angle (0 - 180 degrees)
+        '''
 
         self.servo = board.get_pin('d:%d:s' % pin)
         self.angle = start
@@ -31,30 +40,56 @@ class Joint(object):
         self.stepsize = None
 
     def setTarget(self, target):
+        '''
+        Sets the target angle in degrees for the joint.  Also sets the step size, determined as 2 degrees
+        if the target is more than 100 degrees from the joint's current angle, 1 degree otherwise. 
+        '''
         self.target = target
 
         # XXX need a better formula for step size, probably incorporating time
         self.stepsize = 2 if abs(target-self.angle) > 100 else 1
 
     def atTarget(self):
+        '''
+        Returns True if the joint has reached the target specified by Joint.setTarget(), false otherwise.
+        '''
         return int(self.angle) >= int(self.target)
 
     def moveToTarget(self):
+        '''
+        Moves the joint toward the target specified by Joint.setTarget().
+        '''
         self.angle += self.stepsize * (-1 if self.target-self.angle < 0 else +1)
         self.servo.write(self.angle)
 
 class Arm(object):
 
-    def __init__(self, board, pins, starts):
-        self.joints = [Joint(board, pins[k], starts[k]) for k in range(len(pins))]
+    def __init__(self, board, pins, startAngles):
+        '''
+        Creates a multi-jointed robot arm.  
+        board       - a PyFirmata Board object
+        pins        - Arduino pins to which the arm's servos are connected
+        startAngles - initial servo angles (0 - 180 degrees)
+        '''
+        self.joints = [Joint(board, pins[k], startAngles[k]) for k in range(len(pins))]
 
     def setTargets(self, targets):
+        '''
+        Sets the target angles in degrees for the arm's joints.  Also sets the step sizes, determined as 2 degrees
+        if the target is more than 100 degrees from the joint's current angle, 1 degree otherwise. 
+        '''
         for joint,target in zip(self.joints,targets):
             joint.setTarget(target)
 
     def atTargets(self):
+        '''
+        Returns True if the arm's joints have reached the targets specified by Arm.setTargets(), false otherwise.
+        '''
         return all(joint.atTarget() for joint in self.joints)
 
     def moveToTargets(self):
+        '''
+        Moves the arm's joints toward the targets specified by Arm.setTargets().
+        '''
         for joint in self.joints:
             joint.moveToTarget()
